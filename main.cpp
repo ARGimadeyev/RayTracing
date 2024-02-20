2d9cab7d622caeaacdf326332fdf861c07ae055f
Amir Gimadeev <ARGimadeyev@stud.kpfu.com>
2024-02-20 23:03:46 +0300

    popitka 2

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+void solve();
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    ll t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
+
+void solve() {
+    cout << 1;
+}
